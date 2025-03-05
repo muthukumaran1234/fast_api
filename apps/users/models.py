ce39a8863751cca191c813527d9e95e5e982721d
vishalakshi <vishalakshi.k@medyaan.com>
2025-03-05 13:00:46 +0530

    [ADD]migrate the models

---
+++ apps/users/models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from core.database import Base
+from sqlalchemy.orm import relationship
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+
+    role_mappings = relationship("RoleMapping", back_populates="user_obj")
+
+class RoleMaster(Base):
+    __tablename__ = "role_master"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+    description = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    modified_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    modified_by = Column(Integer, nullable=True)
+
+    role_mappings = relationship("RoleMapping", back_populates="role_obj")
+
+class RoleMapping(Base):
+    __tablename__ = "role_mapping"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    role_id = Column(Integer, ForeignKey("role_master.id"), nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    created_by = Column(Integer, nullable=True)
+    modified_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    modified_by = Column(Integer, nullable=True)
+
+    user_obj = relationship("User", back_populates="role_mappings")
+    role_obj = relationship("RoleMaster", back_populates="role_mappings")
--- core/database.py
+++ core/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-
-DATABASE_URL = "postgresql+psycopg2://postgres.uicdyanzztdwumhxqzhl:Medyaan@12345@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+from sqlalchemy.ext.declarative import declarative_base
+DATABASE_URL = "postgresql+psycopg2://postgres.uicdyanzztdwumhxqzhl:cbemlad01jElgxxm@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 
 # Create Sync Engine
 engine = create_engine(DATABASE_URL, echo=True)
-
+Base = declarative_base()
 # Create Sync Session
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, class_=Session)
 
@@ -16,4 +16,4 @@ def get_db():
         yield db
     finally:
         db.close()
-        
+
